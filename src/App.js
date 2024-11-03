88b516550279042cedcf4f85cb1343340249d4fb
AliakseiKrasko <alekskrasko1985@gmail.com>
2024-11-03 12:30:27 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import {NewComponent} from "./components/NewComponent.tsx";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+    // const students = [
+    //     {id: 1, name: "James", age: 8},
+    //     {id: 2, name: "Robert", age: 18},
+    //     {id: 3, name: "John", age: 28},
+    //     {id: 4, name: "Michael", age: 38},
+    //     {id: 5, name: "William", age: 48},
+    //     {id: 6, name: "David", age: 58},
+    //     {id: 7, name: "Richard", age: 68},
+    //     {id: 8, name: "Joseph", age: 78},
+    //     {id: 9, name: "Thomas", age: 88},
+    //     {id: 10, name: "Charles", age: 98},
+    //     {id: 11, name: "Christopher", age: 100},
+    // ]
+
+
+    const topCars = [
+        {manufacturer: 'BMW', model: 'X5', year: 2008, odometr: 25000 , condition: 'good'},
+        {manufacturer: 'Mersedes', model: 'MLS', year: 2008, odometr: 25000 , condition: 'good'},
+        {manufacturer: 'Audi', model: 'Q7', year: 2008, odometr: 25000 , condition: 'good' },
+        {manufacturer: 'Ford', model: 'Kuga', year: 2008, odometr: 25000 , condition: 'good' },
+        {manufacturer: 'VW', model: 'Polo', year: 2008, odometr: 25000 , condition: 'good' },
+        {manufacturer: 'Subaru', model: 'Forester', year: 2008, odometr: 25000 , condition: 'good' },
+        {manufacturer: 'Isuzu', model: 'Dragon', year: 2008, odometr: 25000 , condition: 'good' },
+        {manufacturer: 'Opel', model: 'Kadet', year: 2008, odometr: 25000 , condition: 'good' },
+        {manufacturer: 'Renault', model: 'Fufel', year: 2008, odometr: 25000 , condition: 'good' }
+    ]
+
+    return (
+        <div className="App">
+            <NewComponent topCars={topCars}/>
+        </div>
+    );
 }
 
 export default App;
